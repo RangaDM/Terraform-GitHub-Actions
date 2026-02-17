8da0489ed01e0a811181827a44118bb1640deb60
Ranga Dananjaya <rangamaithreepala@gmail.com>
2026-02-17 12:13:30 +0530

    Remove default values from Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,14 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "Tag name for the instance"
   type        = string
-  default     = "github-actions-test-ec2"
 }
